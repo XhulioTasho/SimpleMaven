77db31c220b75d26a1ea6dbc32a5e42d56a28e1b
xhuliotasho <xhulio.tasho@gmail.com>
2020-03-12 21:01:22 +0100

    New case Xhulio

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "HELLO WORLD , MY NAME IS XHULIO!!";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
